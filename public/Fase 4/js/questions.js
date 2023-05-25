165a2445db3870b12c98efc24a328383efb09b23
YmenaBo <124740497+YmenaBo@users.noreply.github.com>
2023-05-25 17:24:14 -0500

    V1.1

---
+++ public/Fase 4/js/questions.js
@@ -0,0 +1,58 @@
+// Creacion de una array y pasando el número, preguntas, opciones y respuestas
+let questions = [
+    {
+    numb: 1,
+    question: "¿Qué ventajas ofrece CSS Grid en comparación con el uso de frameworks de diseño como Bootstrap?",
+    answer: "Todas las anteriores",
+    options: [
+      "Ofrece una mayor cantidad de funcionalidades integradas",
+      "Permite una mayor personalización de los diseños",
+      "Mejora la accesibilidad para usuarios con discapacidades",
+      "Todas las anteriores"
+    ]
+  },
+    {
+    numb: 2,
+    question: "¿Cuál es la diferencia entre CSS Grid y otras tecnologías de diseño como Flexbox y Floats?",
+    answer: "CSS Grid es bidimensional, mientras que Flexbox y Floats son unidimensionales",
+    options: [
+      "CSS Grid es bidimensional, mientras que Flexbox y Floats son unidimensionales",
+      "CSS Grid es más fácil de utilizar que Flexbox y Floats",
+      "CSS Grid es más compatible con diseños responsivos que Flexbox y Floats",
+      "Flexbox y Floats permiten crear diseños más complejos y sofisticados que CSS Grid"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Cuál es uno de los beneficios de utilizar CSS Grid?",
+    answer: "Crear diseños complejos y personalizados",
+    options: [
+      "Reducir el tiempo de carga de la página",
+      "Proporcionar compatibilidad con navegadores antiguos",
+      "Crear diseños complejos y personalizados",
+      "Mejorar la accesibilidad de la página para usuarios con discapacidades"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Cómo se adaptan los elementos dentro de la cuadrícula de CSS Grid al tamaño de la pantalla del usuario?",
+    answer: "Se ajustan automáticamente",
+    options: [
+      "Se ajustan manualmente",
+      "Se ajustan automáticamente",
+      "No se ajustan al tamaño de la pantalla del usuario",
+      "Se ajustan solo a tamaños de pantalla específicos"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Es CSS Grid compatible con diseños responsivos?",
+    answer: "Sí, CSS Grid es compatible con diseños responsivos",
+    options: [
+      "No, CSS Grid solo funciona con diseños estáticos",
+      "Depende del tipo de contenido que se esté mostrando en la página",
+      "No, CSS Grid solo funciona con diseños estáticos",
+      "Sí, CSS Grid es compatible con diseños responsivos"
+    ]
+  },
+];
